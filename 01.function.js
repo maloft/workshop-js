d725b6904255321953fce9ecd9c70c6b574eb277
Mohamed Amine ELGNAOUI <mohamed.amine.elgnaoui@gmail.com>
2020-07-23 12:56:26 +0200

    TP01 Les fonctions : Définition d'une fonction et Variable de type fonction

---
+++ 01.function.js
@@ -0,0 +1,27 @@
+console.log("01 - Fonctions");
+
+var nombre1 = 10;
+var nombre2 = 20;
+
+function additionner(nb1, nb2){
+    return nb1 + nb2;
+}
+
+resultat1 = additionner(nombre1, nombre2);
+
+console.log("resultat1 = " + resultat1);
+
+var somme = additionner;
+
+resultat2 = somme(nombre1, nombre2);
+
+console.log("resultat2 = " + resultat2);
+
+
+var multiplication = function(nb1, nb2){
+    return nb1 * nb2;
+}
+
+resultat3 = multiplication(nombre1, nombre2);
+
+console.log("resultat3 = " + resultat3);
