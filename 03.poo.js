779313968d0b4e2aff91075a6f4652875dc209ad
Mohamed Amine ELGNAOUI <mohamed.amine.elgnaoui@gmail.com>
2020-07-23 16:39:32 +0200

    TP03 POO : Suite

--- 03.poo.js
+++ 03.poo.js
@@ -5,7 +5,7 @@ function Personne(nom, prenom, pseudo){
     this.pseudo = pseudo;
 
     this.getNomComplet = function(){
-        return nom + " " + prenom + " " + pseudo;
+        return this.nom + " " + this.prenom + " " + this.pseudo;
     }
 }
 
@@ -21,8 +21,52 @@ console.log(jules.prenom);
 console.log(jules.pseudo);
 console.log(jules.getNomComplet());
 
-// Affichage des données de l'objet paul
-console.log(paul.nom);
-console.log(paul.prenom);
-console.log(paul.pseudo);
-console.log(paul.getNomComplet());
+// Affichage des données de l'objet paul en utilisant une fonction réutilisable
+afficherPersonne = function(personne){
+    console.log(personne.nom + " " + personne.prenom + " " + personne.pseudo + "\n" + personne.getNomComplet());
+}
+
+afficherPersonne(paul);
+
+//Modifier le pseudo de l’objet jules en jules44
+jules.pseudo = "jules44";
+console.log(jules.getNomComplet());
+
+//Ajouter une propriété à Personne
+console.log(jules.age); //undefined
+
+Personne.prototype.age = "NON RENSEIGNE";
+
+console.log(jules.age); //NON RENSEIGNE
+
+jules.age = 30;
+
+console.log(jules.age); //30
+
+//Ajouter une méthode à Personne
+Personne.prototype.getInitiales = function(){
+    return this.nom.charAt(0) + "." + this.prenom.charAt(0);
+}
+
+console.log(jules.getInitiales());
+
+//Objet sans fonction constructeur
+var robert = {nom: "LEPREFET", prenom: "Robert", pseudo: "robert77", getNomComplet: function(){
+    return this.nom + " " + this.prenom + " " + this.pseudo;
+}}
+afficherPersonne(robert);
+
+//Héritage via une fonction constructeur
+function Client(nom, prenom, pseudo, numeroClient){
+    Personne.call(this, nom, prenom, pseudo);
+    this.numeroClient = numeroClient;
+    this.getInfos = function(){
+        return this.numeroClient + " " + this.nom + " " + this.prenom;
+    }
+
+}
+
+var steve = new Client("LUCAS", "Steve", "steve44", "A01");
+afficherPersonne(steve);
+console.log(steve.numeroClient);
+console.log(steve.getInfos());
